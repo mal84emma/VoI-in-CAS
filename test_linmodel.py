d12dfcc52007439a0e89db801ca4512f12dcb0a1
mal84emma <mal84@cam.ac.uk>
2023-06-28 16:26:21 +0100

    Rejig order of ops for full design problem

--- test_linmodel.py
+++ test_linmodel.py
@@ -10,8 +10,9 @@ from linmodel import LinProgModel
 
 dataset_dir = os.path.join('A37_example_validate') # dataset directory
 schema_path = os.path.join('data', dataset_dir, 'schema.json')
-tau = 24*7*4 # model prediction horizon (number of timesteps of data predicted)
-opex_factor = (24*7*52*20)/tau
+#tau = 24*7*52 # model prediction horizon (number of timesteps of data predicted)
+#opex_factor = (24*7*52*20)/tau
+opex_factor = 10
 
 # Initialise CityLearn environment object.
 env = CityLearnEnv(schema=schema_path)
@@ -19,12 +20,11 @@ env = CityLearnEnv(schema=schema_path)
 # Initialise Linear MPC object.
 lp = LinProgModel(env=env)
 #for i in range(2): lp.add_env(env=env)
-lp.tau = tau
-lp.generate_LP(clip_level='b',design=True,pricing_dict={'carbon':5e-1,'battery':1e3,'solar':2e3},opex_factor=opex_factor)
-
+#lp.tau = tau
 lp.set_time_data_from_envs()
+lp.generate_LP(clip_level='b',design=True,pricing_dict={'carbon':5e-1,'battery':1e3,'solar':2e3},opex_factor=opex_factor)
 lp.set_LP_parameters()
-results = lp.solve_LP(verbose=True)
+results = lp.solve_LP(verbose=True,ignore_dpp=True)
 
 print(results['objective'])
 print(results['objective_contrs'])
